5d394c9f7bc115891b447c902574efc47475b993
Milly <liuhongmein@gmail.com>
2022-11-10 16:53:34 -0500

    state to reducer

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -2,7 +2,7 @@ import React from "react";
 import Todo from "./Todo";
 
 const TodoList = ({ todos, handleCompleteTodos, handleCancelTodo }) => {
-  return todos.map((todo) => {
+  return todos?.map((todo) => {
     return (
       <Todo
         todo={todo}
